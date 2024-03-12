590c167055983694b1ca0852bf2841b49ce384bd
Jamez213 <dphillipjames@yahoo.com>
2024-03-12 18:22:54 +0800

    added some href for the icon and change their color to black.

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -33,12 +33,20 @@ const LoginForm = () => {
                 </div>
 
                 <div className='account-register'> 
-                        <div className='account-icon'><FaFacebook/></div>
-                        <div className='account-icon'><FaGoogle /></div>
-                        <div className='account-icon'><FaGithub /></div>
+                    <a href="https://www.facebook.com" target='_blank'><div className='account-icon'><FaFacebook /></div></a>
+                    <a href="https://www.google.com" target='_blank'><div className='account-icon'><FaGoogle /></div></a>
+                    <a href="https://github.com" target='_blank'><div className='account-icon'><FaGithub /></div></a>
                 </div>
             </form>
 
+
+            <div className='footer'>
+            <footer>
+                <div class="copyright">
+                    <p>&copy; 2024 James Damas. All rights reserved.</p>
+                </div>
+            </footer>
+            </div>
         </div>
     )
 }
